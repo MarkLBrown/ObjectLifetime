1ff0b921e68165f55377349f83cbbcbe4f420cc2
MarkLBrown <Markliambrown@hotmail.com>
2018-10-20 19:11:28 +0100

    Add project files.

---
+++ ObjectLifetime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+    } 
+        
+}
